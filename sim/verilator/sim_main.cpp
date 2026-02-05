55630ed7d6dd7ab1e2ce8aa0dcffca0455bda7b2
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2026-02-05 14:45:35 +0900

    wavedromの波形記述を修正

--- sim/verilator/sim_main.cpp
+++ sim/verilator/sim_main.cpp
@@ -13,16 +13,16 @@ int main(int argc, char** argv) {
     // 行バッファにしてテスト進行/失敗理由を即時に出力する。
     setvbuf(stdout, nullptr, _IOLBF, 0);
     setvbuf(stderr, nullptr, _IONBF, 0);
-    
+
     Vtb_adat_rx* top = new Vtb_adat_rx;
-    
+
 #if VM_TRACE
     Verilated::traceEverOn(true);
     VerilatedFstC* tfp = new VerilatedFstC;
     top->trace(tfp, 99);
     tfp->open("adat_rx.fst");
 #endif
-    
+
     while (!Verilated::gotFinish()) {
         top->eval();
 #if VM_TRACE
@@ -36,6 +36,6 @@ int main(int argc, char** argv) {
     delete tfp;
 #endif
     delete top;
-    
+
     return 0;
 }
